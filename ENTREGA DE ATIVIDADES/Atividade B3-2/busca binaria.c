e9392c546cb7bac59e95ab44b9997c4590369d2e
Felipe Torquato <112289573+FELPSTS@users.noreply.github.com>
2024-08-26 23:18:49 -0300

    Update busca binaria.c

--- ENTREGA DE ATIVIDADES/Atividade B3-2/busca binaria.c
+++ ENTREGA DE ATIVIDADES/Atividade B3-2/busca binaria.c
@@ -1,14 +1,35 @@
-def busca_binária(A, x):
-    início = 0
-    fim = len(A) - 1
+#include <stdio.h>
 
-    while início <= fim:
-        meio = (início + fim) // 2
-        if A[meio] == x:
-            return meio
-        elif A[meio] < x:
-            início = meio + 1
-        else:
-            fim = meio - 1
+int busca_binaria(int A[], int n, int x) {
+    int inicio = 0;
+    int fim = n - 1;
 
-    return -1
+    while (inicio <= fim) {
+        int meio = (inicio + fim) / 2;
+
+        if (A[meio] == x) {
+            return meio;
+        } else if (A[meio] < x) {
+            inicio = meio + 1;
+        } else {
+            fim = meio - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    int A[] = {2, 3, 4, 10, 40};
+    int x = 10;
+    int n = sizeof(A) / sizeof(A[0]);
+
+    int resultado = busca_binaria(A, n, x);
+    if (resultado != -1) {
+        printf("Elemento encontrado no índice %d\n", resultado);
+    } else {
+        printf("Elemento não encontrado\n");
+    }
+
+    return 0;
+}
